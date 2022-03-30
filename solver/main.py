fb0087188a2e8524d1409262c59d0b5faebb74f0
Vladibas <vlad7772004@gmail.com>
2022-03-30 22:33:51 +0300

    Added rescanning if cube was scanned incorrect.

--- solver/main.py
+++ solver/main.py
@@ -9,8 +9,11 @@ from display import Display
 
 raw_input = [[[(49, 59, 12), (61, 93, 100), (50, 59, 12)], [(59, 91, 100), (7, 33, 11), (59, 86, 100)], [(46, 56, 12), (59, 89, 100), (42, 50, 11)]], [[(23, 9, 4), (43, 19, 6), (24, 9, 3)], [(26, 10, 5), (69, 100, 100), (26, 10, 4)], [(23, 9, 4), (40, 19, 7), (21, 9, 4)]], [[(5, 29, 9), (6, 24, 52), (5, 30, 9)], [(6, 33, 11), (48, 21, 8), (6, 33, 10)], [(6, 29, 9), (6, 25, 53), (5, 29, 9)]], [[(35, 15, 5), (26, 10, 5), (37, 16, 5)], [(40, 18, 7), (47, 59, 14), (41, 18, 7)], [(36, 16, 5), (27, 10, 4), (38, 17, 6)]], [[(5, 20, 47), (6, 33, 11), (5, 21, 48)], [(5, 23, 52), (30, 11, 6), (6, 24, 52)], [(5, 21, 48), (6, 34, 11), (5, 22, 49)]], [[(54, 82, 100), (55, 67, 14), (55, 83, 100)], [(53, 66, 15), (7, 25, 54), (55, 66, 14)], [(46, 68, 94), (51, 61, 13), (49, 72, 98)]]]
 
-def assemble_the_cube():
+def assemble_the_cube() -> None:
 	solving_steps = solver.solve()
+	print(f"Len of solving steps: {len(solving_steps)}")
+	print("Solving steps:\n", solving_steps)
+
 	for i, step in enumerate(solving_steps[:]):
 		side_code, n, byclockwise = map(int, list(step))
 		for _ in range(n):
@@ -18,17 +21,18 @@ def assemble_the_cube():
 			display.rotate_side(side_code=side_code, byclockwise=bool(byclockwise))
 
 sides_map = rgb_to_color_name(raw_input)
+
 rubcube = RubiksCube(sides_map)
-
 solver = Solver(rubcube=rubcube)
-
-solving_steps = solver.solve()
-print(f"Len of solving steps: {len(solving_steps)}")
-print("Solving steps:\n", solving_steps)
-
-display = Display(rubcube=solver.rubcube)
+display = Display(rubcube=rubcube)
 
 assembler = threading.Thread(target=assemble_the_cube)
-
 assembler.start()
-display.run()
+
+restart = True
+while restart:
+	try:
+		display.run()
+		restart = False
+	except:
+		print("EXCEPTION!")
--- solver/network.py
+++ solver/network.py
@@ -8,6 +8,22 @@ from datatypes import RubiksCube
 from solver import Solver
 from display import Display
 
+
+def validation(rubcube:RubiksCube) -> bool:
+	"""Validates that rubcube was assembled correct."""
+	for side_idx in range(6):
+		central_cube_color = rubcube.sides_map[side_idx][1][1].color
+		for row_idx in range(3):
+			for col_idx in range(3):
+				if row_idx == col_idx == 1:
+					continue
+
+				color = rubcube.sides_map[side_idx][row_idx][col_idx].color
+				if color != central_cube_color:
+					return False
+
+	return True
+
 def assemble_the_cube(solving_steps:list, conn:socket.socket, addr:tuple) -> None:
 	"""
 	Solves the Rubik's cube by solving_steps.
@@ -23,6 +39,7 @@ def assemble_the_cube(solving_steps:list, conn:socket.socket, addr:tuple) -> Non
 		side_code, n, byclockwise = map(int, list(step))
 		conn.sendto(step.encode(), addr)
 		for _ in range(n):
+			time.sleep(1)
 			display.rotate_side(side_code=side_code, byclockwise=bool(byclockwise))
 
 		resp = conn.recv(1024)
@@ -58,26 +75,42 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		conn.sendto("scan".encode(), addr)
 
 		# Gets raw colors (red, green blue at percents)
-		raw_colors = conn.recv(1024)
-		if not raw_colors:
-			print("[ Colors didn't recived ]")
-			s.close()
-			sys.exit()
+		while True:
+			raw_colors = conn.recv(1024)
+			if not raw_colors:
+				print("[ Colors didn't recived ]")
+				s.close()
+				sys.exit()
 
-		print("[ Colors recived! ]")
-		raw_colors = json.loads(raw_colors.decode())
-		# print(raw_colors)
-		sides_map = rgb_to_color_name(raw_colors)
+			print("[ Colors recived! ]")
+			raw_colors = json.loads(raw_colors.decode())
+			sides_map = rgb_to_color_name(raw_colors)
 
-		rubcube = RubiksCube(sides_map)
-		solver = Solver(rubcube)
-		solving_steps = solver.solve()
+			rubcube = RubiksCube(sides_map)
+			solver = Solver(rubcube)
+			try:
+				# If solver.solve() raise exception then cube was scanned incorrectly
+				solving_steps = solver.solve()
+			except:
+				# Cube was scanned incorrectly
+				print("[ Rescanning ]")
+				conn.sendto("rescan".encode(), addr)
+				continue
+
+			if validation(solver.rubcube):
+				# Everything is awesome
+				break
+			else:
+				# Cube was scanned incorrectly
+				print("[ Rescanning ]")
 
 		display = Display(rubcube)
 
+		# Visual assembler starts at thread
 		assembler = threading.Thread(target=assemble_the_cube, args=(solving_steps, conn, addr))
 		assembler.start()
 
+		# Launching display visualisation
 		restart = True
 		while restart:
 			try:
